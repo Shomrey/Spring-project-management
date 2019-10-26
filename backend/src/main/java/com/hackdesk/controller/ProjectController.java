17aa097053b6db563e0ee55f5d9af5865a9efc87
Lukasz Kowalski <lukowalski97@gmail.com>
2019-10-26 14:19:09 +0200

    created flyway init script
    Script creates every default table in database

--- backend/src/main/java/com/hackdesk/controller/ProjectController.java
+++ backend/src/main/java/com/hackdesk/controller/ProjectController.java
@@ -1,15 +1,28 @@
 package com.hackdesk.controller;
 
 
+import com.hackdesk.model.Project;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProjectController {
 
     @PostMapping("/backend/projects")
-    Project addProject(){
+    Project addProject(@RequestBody Project project){
 
+
+        return null;
+    }
+
+    @GetMapping("/backend/projects")
+    List<Project> getAllProjects(){
+
+        return null;
     }
 
 
