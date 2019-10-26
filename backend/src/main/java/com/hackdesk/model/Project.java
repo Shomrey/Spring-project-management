420848269b9b564d0e9b2943bd75d7e7071a1751
Mervolt <michal.makson@gmail.com>
2019-10-26 16:14:29 +0200

    database config

--- backend/src/main/java/com/hackdesk/model/Project.java
+++ backend/src/main/java/com/hackdesk/model/Project.java
@@ -1,20 +1,26 @@
 package com.hackdesk.model;
 
+import jdk.jfr.Timestamp;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-
+@Entity
+@Table(name = "project")
 public class Project {
     @Id
     @GeneratedValue
+    @Column(name = "project_id")
     Integer projectID;
+    @Column(name = "project_name")
     String projectName;
-    Date projectDeadline;
+    @Column(name = "project_deadline_date")
+    Timestamp projectDeadline;
     @OneToMany(cascade=CascadeType.ALL)
     @JoinTable(name ="project_task", joinColumns = @JoinColumn(name = "task_id"), inverseJoinColumns = @JoinColumn(name = "project_id"))
     private Collection<Task> tasks = new ArrayList<>();
-    public Project(String projectName, Date projectDeadline){
+    public Project(String projectName, Timestamp projectDeadline){
         this.projectName = projectName;
         this.projectDeadline = projectDeadline;
     }
--- backend/src/main/java/com/hackdesk/model/ProjectDAO.java
+++ backend/src/main/java/com/hackdesk/model/ProjectDAO.java
@@ -1,16 +1,32 @@
 package com.hackdesk.model;
 
-import java.util.Date;
+import com.hackdesk.DbConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+@Component
 public class ProjectDAO {
-    private static final QUERY_INSERT_INTO = "INSERT INTO VALUES";
+    private static final String SQL_INSERT = "INSERT VALUES ";
+    private static final String SQL_INTO = " INTO";
+    private static final String TABLE_NAME = " project";
+    @Autowired
+    private DbConfiguration dbConfiguration;
 
     Project getProject(String projectName){
         return null;
     }
-    boolean addProject(String projectName, Date projectDeadline){
-        String prepareQueryString = "INSERT INTO VALUES"
-        return false;
+    boolean addProject(Project project){
+
+        Session session = dbConfiguration.sessionFactory().getConfiguration().buildSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(project);
+        transaction.commit();
+        return true;
     }
     Project[] getAllProjects(){
         return null;
